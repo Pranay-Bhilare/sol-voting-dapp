6cf2c3ec7e7d1809579356fed770519cce6dc6ac
bhilare_ <bhilarespranay81@gmail.com>
2025-04-12 19:53:58 +0530

    Voting part implemented.

--- anchor/programs/solvotingdapp/src/lib.rs
+++ anchor/programs/solvotingdapp/src/lib.rs
@@ -23,13 +23,21 @@ pub mod solvotingdapp {
 
     pub fn initialize_candidate(ctx:Context<InitializeCandidate>,
                                 candidate_name: String,
-                                poll_id: u64) -> Result<()> { 
+                                _poll_id: u64) -> Result<()> { 
       let candidate = &mut ctx.accounts.candidate;
       candidate.candidate_name = candidate_name;
       candidate.candidate_votes = 0;
-
+      let poll = &mut ctx.accounts.poll;
+      poll.candidate_amount += 1;                            
       Ok(())
+    }
 
+    pub fn vote(ctx:Context<Vote>,
+                _candidate_name: String,
+                _poll_id: u64) -> Result<()> {
+       let candidate = &mut ctx.accounts.candidate;
+       candidate.candidate_votes += 1;
+       Ok(())
     }
 }
 
@@ -65,6 +73,7 @@ pub struct InitializeCandidate<'info>{
     #[account(mut)]
     pub signer: Signer<'info>,
     #[account (
+        mut,
         seeds = [poll_id.to_le_bytes().as_ref()],
         bump
     )]
@@ -86,4 +95,21 @@ pub struct Candidate{
   #[max_len(32)]
   pub candidate_name : String,
   pub candidate_votes : u64,
+}
+
+#[derive(Accounts)]
+#[instruction(candidate_name:String, poll_id: u64)]
+pub struct Vote<'info>{
+    pub signer: Signer<'info>,
+    #[account (
+        mut,
+        seeds = [poll_id.to_le_bytes().as_ref()],
+        bump
+    )]
+    pub poll: Account<'info, Poll>,
+    #[account (
+        seeds = [poll_id.to_le_bytes().as_ref(), candidate_name.as_bytes()],
+        bump
+    )]
+    pub candidate: Account<'info, Candidate>
 }
